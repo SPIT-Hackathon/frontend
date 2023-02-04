1fd359a0eb69502c231f6bc3d2f2a1db8a309bd7
Gaurav Yashwant Parulekar <gaurav.parulekar@gupshup.io>
2023-02-04 22:10:38 +0530

    dnd

--- src/components/Home/Category.js
+++ src/components/Home/Category.js
@@ -7,9 +7,9 @@ const Category = () => {
     <div className={styles.category}>
       <p>Browse Categories</p>
       <div className={styles.subcat}>
-        <Link href="/transcript"><div className={`${styles.cat} ${styles.red}`}>Get Notes</div></Link>
+        <Link href="/transcript"><div className={`${styles.cat} ${styles.red}`}>Notes</div></Link>
         <Link href="/transcript"><div className={`${styles.cat} ${styles.blue}`}>OCR</div></Link>
-        <Link href="/transcript"><div className={`${styles.cat} ${styles.purple}`}>Cat 3</div></Link>
+        <Link href="/dnd"><div className={`${styles.cat} ${styles.purple}`}>Do not Distrub</div></Link>
         <Link href="/transcript"><div className={`${styles.cat} ${styles.red}`}>Cat 4</div></Link>
       </div>
     </div>
