24e40793eeb4b7a501cdb47eaf6829e4c0b55f30
Gaurav Yashwant Parulekar <gaurav.parulekar@gupshup.io>
2023-02-04 18:42:37 +0530

    Transcripts chunks

--- src/pages/transcript.jsx
+++ src/pages/transcript.jsx
@@ -3,6 +3,7 @@ import styles from "../styles/Transcript.module.css";
 import axios from "axios";
 import dataContext from "@/hooks/DataContext/dataContext";
 import authContext from "@/hooks/AuthContext/authContext";
+import TransHistory from "@/components/transcript/TransHistory";
 
 const transcript = () => {
   const { user } = useContext(authContext);
@@ -25,42 +26,45 @@ const transcript = () => {
   };
 
   return (
-    <div className={styles.transcript}>
-      <div className={styles.subhome}>
-        <input
-          onChange={(e) => setLink(e.target.value)}
-          type="text"
-          placeholder="Please enter YouTube video link"
-        />
-        <div className={styles.option}>
-          <select
-            onChange={(e) => setSubject(e.target.value)}
-            value={subject}
-            name="subject"
-            id="subjecr"
-          >
-            <option value="" disabled>Select Subject</option>
-            {initialSubs.map((sub) => {
-              return <option value={sub}>{sub}</option>;
-            })}
-          </select>
-          <input
-            onChange={(e) => setChapter(e.target.value)}
-            type="text"
-            placeholder="Enter Chapter name"
-          />
-        </div>
-        <iframe
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${yid}`}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-        <button onClick={() => handleSubmit()}>Get Notes</button>
-      </div>
+    // <div className={styles.transcript}>
+    //   <div className={styles.subhome}>
+    //     <input
+    //       onChange={(e) => setLink(e.target.value)}
+    //       type="text"
+    //       placeholder="Please enter YouTube video link"
+    //     />
+    //     <div className={styles.option}>
+    //       <select
+    //         onChange={(e) => setSubject(e.target.value)}
+    //         value={subject}
+    //         name="subject"
+    //         id="subjecr"
+    //       >
+    //         <option value="" disabled>Select Subject</option>
+    //         {initialSubs.map((sub) => {
+    //           return <option value={sub}>{sub}</option>;
+    //         })}
+    //       </select>
+    //       <input
+    //         onChange={(e) => setChapter(e.target.value)}
+    //         type="text"
+    //         placeholder="Enter Chapter name"
+    //       />
+    //     </div>
+    //     <iframe
+    //       width="560"
+    //       height="315"
+    //       src={`https://www.youtube.com/embed/${yid}`}
+    //       title="YouTube video player"
+    //       frameborder="0"
+    //       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    //       allowfullscreen
+    //     ></iframe>
+    //     <button onClick={() => handleSubmit()}>Get Notes</button>
+    //   </div>
+    // </div>
+    <div>
+      <TransHistory/>
     </div>
   );
 };
