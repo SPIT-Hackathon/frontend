bc7dbe72395f0d576df8565ec470155fe23401a3
Hrishikesh Lamdade <hrishilamdade@gmail.com>
2023-02-05 10:00:09 +0530

    axios client

--- src/services/axios-client.js
+++ src/services/axios-client.js
@@ -3,12 +3,15 @@ import axios from "axios";
 const SERVER_BASE_URL =
   "https://j0sal-probable-trout-wx5x97wrxg39w7r-5000.preview.app.github.dev/";
 
+const SERVER_BASE_URL2 =
+  "https://hrishilamdade-laughing-space-train-6w4g95gv4xwfxv5v-5000.preview.app.github.dev/";
+
 const LOCAL_SERVER_URL = "http://127.0.0.1:5000/";
 
 const COLAB_SERVER_URL = "http://4fa7-35-194-69-118.ngrok.io/";
 
 const axiosClient = axios.create({
-  baseURL: SERVER_BASE_URL,
+  baseURL: SERVER_BASE_URL2,
 });
 
 export default axiosClient;
