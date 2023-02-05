c9416679f8e93bf27e1e02a81738a50bf99cb9db
J0SAL <joyalmeida321@gmail.com>
2023-02-05 09:51:25 +0530

    updated code

--- src/components/Home/SideHug.js
+++ src/components/Home/SideHug.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import styles from "../../styles/SideHug.module.css"
+import React from "react";
+import styles from "../../styles/SideHug.module.css";
 
 const SideHug = () => {
   return (
     <div className={styles.sidehug}>
       <div className={styles.side_image}>
-        <img src="/assets/images/sidehug_image.jpg" alt="" width={500}/>
+        <img src="/assets/images/sidehug_image.jpg" alt="" width={500} />
       </div>
       <div className={styles.side_content}>
         <h1 className={styles.title}>StudyPat</h1>
-        <p className={styles.text}>One place for all your productivity</p>
-        </div>
+        <p className={styles.text}>
+          A One-Stop Location for All Your Productivity Needs
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideHug
+export default SideHug;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,17 @@ import { Inter } from "@next/font/google";
 import SideHug from "@/components/Home/SideHug";
 import Category from "@/components/Home/Category";
 import { Container } from "react-bootstrap";
+import { useContext } from "react";
+import authContext from "@/hooks/AuthContext/authContext";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
+  const { user } = useContext(authContext);
   return (
     <>
       <SideHug />
-      <Category />
+
+      {user && <Category />}
     </>
   );
 }
