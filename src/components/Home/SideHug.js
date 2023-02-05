c4ef1589377ee569be03e3e70eba724708c1ad32
Gaurav Yashwant Parulekar <gaurav.parulekar@gupshup.io>
2023-02-05 06:48:20 +0530

    Sidehug

--- src/components/Home/SideHug.js
+++ src/components/Home/SideHug.js
@@ -4,8 +4,13 @@ import styles from "../../styles/SideHug.module.css"
 const SideHug = () => {
   return (
     <div className={styles.sidehug}>
+      <div className={styles.side_image}>
+        <img src="/assets/images/sidehug_image.jpg" alt="" width={500}/>
+      </div>
+      <div className={styles.side_content}>
         <h1 className={styles.title}>StudyPat</h1>
         <p className={styles.text}>One place for all your productivity</p>
+        </div>
     </div>
   )
 }
