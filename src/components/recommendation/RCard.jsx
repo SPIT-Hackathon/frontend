450580c224894dcd206dd73e03a1cc7b12973348
Gaurav Yashwant Parulekar <gaurav.parulekar@gupshup.io>
2023-02-05 07:21:11 +0530

    RCard data

--- src/components/recommendation/RCard.jsx
+++ src/components/recommendation/RCard.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "../../styles/Rcard.module.css";
 
 function RCard({ data }) {
-    return (
-        <div>
-            <h3>{data.title}</h3>
-            <p>{data.link}</p>
-        </div>
-    )
+  const [id, setId] = useState(data.link.split("=")[1]);
+  return (
+    <div className={styles.rcard}>
+      <iframe
+        width="300"
+        height="200"
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <h4>{data.title}</h4>
+    </div>
+  );
 }
 
-export default RCard
+export default RCard;
